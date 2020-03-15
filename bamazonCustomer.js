b8be08ff22c1312c1fe7e575372892e0f0e84887
Ana Pineda <anamichellepd@gmail.com>
2020-03-14 18:36:24 -0700

    got the first display par without it

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,4 +12,27 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadID + "\n");
+  queryAllProducts();
 });
+
+//function to display everything
+function queryAllProducts() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(
+        "Product ID: " +
+          res[i].product_id +
+          " | " +
+          res[i].product_name +
+          " | " +
+          "Price: $" +
+          res[i].price +
+          " | " +
+          "Stock quantity: " +
+          res[i].stock_quantity
+      );
+    }
+    console.log("--------------------------");
+  });
+}
